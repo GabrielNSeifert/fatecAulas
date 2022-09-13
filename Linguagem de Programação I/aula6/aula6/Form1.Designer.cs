2f1a918cb9e0213b420f5c65c259c11aa8d13211
Gabriel Seifert Neves <110490386+GabrielNSeifert@users.noreply.github.com>
2022-09-13 12:39:58 -0300

    Add files via upload

---
+++ Linguagem de Programação I/aula6/aula6/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace aula6
+{
+    partial class Meses
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Meses));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtnum = new System.Windows.Forms.TextBox();
+            this.btexibir = new System.Windows.Forms.Button();
+            this.lblmes = new System.Windows.Forms.Label();
+            this.btexibir2 = new System.Windows.Forms.Button();
+            this.btsair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(32, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(338, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o número:";
+            // 
+            // txtnum
+            // 
+            this.txtnum.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtnum.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtnum.Location = new System.Drawing.Point(426, 87);
+            this.txtnum.Name = "txtnum";
+            this.txtnum.Size = new System.Drawing.Size(325, 54);
+            this.txtnum.TabIndex = 1;
+            // 
+            // btexibir
+            // 
+            this.btexibir.BackColor = System.Drawing.Color.Transparent;
+            this.btexibir.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btexibir.FlatAppearance.BorderColor = System.Drawing.Color.Magenta;
+            this.btexibir.FlatAppearance.BorderSize = 0;
+            this.btexibir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btexibir.ForeColor = System.Drawing.Color.Transparent;
+            this.btexibir.Location = new System.Drawing.Point(817, 80);
+            this.btexibir.Name = "btexibir";
+            this.btexibir.Size = new System.Drawing.Size(276, 71);
+            this.btexibir.TabIndex = 2;
+            this.btexibir.Text = "Exibir Mês";
+            this.btexibir.UseVisualStyleBackColor = false;
+            this.btexibir.UseWaitCursor = true;
+            this.btexibir.Click += new System.EventHandler(this.Btexibir_Click);
+            // 
+            // lblmes
+            // 
+            this.lblmes.AutoSize = true;
+            this.lblmes.BackColor = System.Drawing.Color.Transparent;
+            this.lblmes.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblmes.Location = new System.Drawing.Point(250, 187);
+            this.lblmes.Name = "lblmes";
+            this.lblmes.Size = new System.Drawing.Size(55, 50);
+            this.lblmes.TabIndex = 3;
+            this.lblmes.Text = "...";
+            // 
+            // btexibir2
+            // 
+            this.btexibir2.BackColor = System.Drawing.Color.Transparent;
+            this.btexibir2.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btexibir2.FlatAppearance.BorderColor = System.Drawing.Color.Magenta;
+            this.btexibir2.FlatAppearance.BorderSize = 0;
+            this.btexibir2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btexibir2.ForeColor = System.Drawing.Color.Transparent;
+            this.btexibir2.Location = new System.Drawing.Point(763, 177);
+            this.btexibir2.Name = "btexibir2";
+            this.btexibir2.Size = new System.Drawing.Size(395, 71);
+            this.btexibir2.TabIndex = 4;
+            this.btexibir2.Text = "Exibir Mes (Switch)";
+            this.btexibir2.UseVisualStyleBackColor = false;
+            this.btexibir2.UseWaitCursor = true;
+            this.btexibir2.Click += new System.EventHandler(this.Btexibir2_Click);
+            // 
+            // btsair
+            // 
+            this.btsair.BackColor = System.Drawing.Color.Transparent;
+            this.btsair.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btsair.FlatAppearance.BorderColor = System.Drawing.Color.Magenta;
+            this.btsair.FlatAppearance.BorderSize = 0;
+            this.btsair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btsair.ForeColor = System.Drawing.Color.Transparent;
+            this.btsair.Location = new System.Drawing.Point(763, 286);
+            this.btsair.Name = "btsair";
+            this.btsair.Size = new System.Drawing.Size(395, 71);
+            this.btsair.TabIndex = 5;
+            this.btsair.Text = "Sair";
+            this.btsair.UseVisualStyleBackColor = false;
+            this.btsair.UseWaitCursor = true;
+            this.btsair.Click += new System.EventHandler(this.Btsair_Click);
+            // 
+            // Meses
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(24F, 50F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1222, 639);
+            this.Controls.Add(this.btsair);
+            this.Controls.Add(this.btexibir2);
+            this.Controls.Add(this.lblmes);
+            this.Controls.Add(this.btexibir);
+            this.Controls.Add(this.txtnum);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(12);
+            this.Name = "Meses";
+            this.Text = "Mês";
+            this.Load += new System.EventHandler(this.Meses_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtnum;
+        private System.Windows.Forms.Button btexibir;
+        private System.Windows.Forms.Label lblmes;
+        private System.Windows.Forms.Button btexibir2;
+        private System.Windows.Forms.Button btsair;
+    }
+}
+
---
+++ Linguagem de Programação I/aula6/aula6/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula6
+{
+    public partial class Meses : Form
+    {
+        public Meses()
+        {
+            InitializeComponent();
+        }
+
+        private void Btexibir_Click(object sender, EventArgs e)
+        {
+            int num;
+
+            num = Convert.ToInt16(txtnum.Text);
+
+            if (num == 1)
+            {
+                lblmes.Text = "Janeiro";
+
+            } 
+            else if (num == 2)
+            {
+                lblmes.Text = "Fevereiro";
+            }
+            else if (num == 3)
+            {
+                lblmes.Text = "Março";
+            }
+            else if (num == 4)
+            {
+                lblmes.Text = "Abril";
+            }
+            else if (num == 5)
+            {
+                lblmes.Text = "Maio";
+            }
+            else if (num == 6)
+            {
+                lblmes.Text = "Junho";
+            }
+            else if(num == 7)
+            {
+                lblmes.Text = "Julho";
+            }
+            else if (num == 8)
+            {
+                lblmes.Text = "Agosto";
+            }
+            else if (num == 9)
+            {
+                lblmes.Text = "Setembro";
+            }
+            else if (num == 10)
+            {
+                lblmes.Text = "Outubro";
+            }
+            else if (num == 11)
+            {
+                lblmes.Text = "Novembro";
+            }
+            else if (num == 12)
+            {
+                lblmes.Text = "Dezembro";
+            }
+            else
+            {
+                lblmes.Text = "Esse mês não existe.";
+            }
+        }
+
+        private void Btexibir2_Click(object sender, EventArgs e)
+        {
+
+            int num;
+            num = Convert.ToInt16(txtnum.Text);
+
+            switch (num)
+            {
+                case 1:
+                lblmes.Text = "Janeiro";
+                    break;
+                case 2:
+                    lblmes.Text = "Fevereiro";
+                    break;
+                case 3:
+                    lblmes.Text = "Março";
+                    break;
+                case 4:
+                    lblmes.Text = "Abril";
+                    break;
+                case 5:
+                    lblmes.Text = "Maio";
+                    break;
+                case 6:
+                    lblmes.Text = "Junho";
+                    break;
+                case 7:
+                    lblmes.Text = "Julho";
+                    break;
+                case 8:
+                    lblmes.Text = "Agosto";
+                    break;
+                case 9:
+                    lblmes.Text = "Setembro";
+                    break;
+                case 10:
+                    lblmes.Text = "Outubro";
+                    break;
+                case 11:
+                    lblmes.Text = "Novembro";
+                    break;
+                case 12:
+                    lblmes.Text = "Dezembro";
+                    break;
+                default:
+                    lblmes.Text = "Esse mês não existe!";
+                    break;
+            }
+        }
+
+        private void Meses_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btsair_Click(object sender, EventArgs e)
+        {
+
+            DialogResult resp;
+
+            MessageBox.Show("Obrigado por usar nossos softwares!", "Tmj Familia!!!",MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            resp = MessageBox.Show("Deseja fechar o programa?", "Mensagem!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if(resp == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
